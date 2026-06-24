21aae02964a060fedf1526dc58f91bd69f322379
Matthew Neal <iotaverbum@gmail.com>
2026-06-24 22:18:09 +0200

    Wire basic workbench interactions

--- static/js/workbench.js
+++ static/js/workbench.js
@@ -1,1 +1,11 @@
+function iotaShow(value) {
+  document.getElementById('jsonView').textContent = JSON.stringify(value, null, 2);
+}
+function iotaMessage(text) {
+  var log = document.getElementById('chatLog');
+  var p = document.createElement('p');
+  p.className = 'msg';
+  p.textContent = text;
+  log.appendChild(p);
+}
 console.log('IOTA workbench loaded');
